a19744e6995348a961b4aabaa7329822b2a1c3d4
孙俊威 <sunjunwei@sunjunweideMacBook-Pro.local>
2022-11-21 18:41:59 +0800

    style: opti

--- src/detectPattern.ts
+++ src/detectPattern.ts
@@ -1,6 +1,6 @@
 import { Patterns, DetectFunction } from "./types";
 import { _10K, _100K, _00XX, _0x10K } from "./constants";
-import patternsStrategys from "./PatternsStrategy";
+import patternsStrategys from "./patternsStrategy";
 const { detectDigitPattern, detectLetterPattern, detectDigitalStartwith0x } =
   patternsStrategys;
 
